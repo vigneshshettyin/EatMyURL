73b9809c299ac4a0260a054d6762bd19389f19b1
Shanwill Pinto <shanwillpinto@gmail.com>
2021-12-28 09:07:08 +0530

    Base styling

--- Client - React/v2/src/page/index.js
+++ Client - React/v2/src/page/index.js
@@ -13,20 +13,50 @@ const HomePage = () => {
       <ParentWrapper>
         <ChildWrapper>
           <LinkWrapper>
-            <h2>EatMyURL</h2>
-            <TextField id="outlined-basic" label="URL" variant="outlined" />
-            <TextField
-              id="outlined-basic"
-              label="Short ID"
-              variant="outlined"
-            />
+            <h2 className="logo">EatMyURL</h2>
+            <div className="linkform">
+              <div className="input-fields">
+                <TextField
+                  id="outlined-basic"
+                  label="URL"
+                  variant="outlined"
+                  className="URLfield"
+                  style={{ width: "20vw" }}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Short ID"
+                  variant="outlined"
+                />
+              </div>
 
-            <Button variant="contained" size="large">
-              Lemme eat it !!
-            </Button>
-            <Button variant="outlined" size="large">
-              Who’s visited my link?
-            </Button>
+              <div className="button-area">
+                <Button
+                  variant="contained"
+                  size="large"
+                  style={{
+                    "background-color": "black",
+                    "border-radius": "10px",
+                    padding: "20px 50px 20px 50px",
+                    "font-size": "1.25rem",
+                  }}
+                >
+                  Lemme eat it !!
+                </Button>
+                <Button
+                  variant="contained"
+                  size="large"
+                  style={{
+                    "background-color": "black",
+                    "border-radius": "10px",
+                    padding: "20px 50px 20px 50px",
+                    "font-size": "1.25rem",
+                  }}
+                >
+                  Who’s visited my link?
+                </Button>
+              </div>
+            </div>
           </LinkWrapper>
           <DocsWrapper>
             <h2>Docs Section</h2>
--- Client - React/v2/src/styles/layout-styles.js
+++ Client - React/v2/src/styles/layout-styles.js
@@ -13,7 +13,7 @@ const ChildWrapper = styled.div`
   width: 90vw;
   height: 90vh;
   background-color: white;
-  border: 2px solid black;
+  border: 4px solid black;
   border-radius: 20px;
   display: flex;
   align-items: center;
@@ -24,11 +24,51 @@ const LinkWrapper = styled.div`
   @media (max-width: 768px) {
     width: 90vw;
     height: 90vh;
+    padding: 0 10px 0px 10px;
+    .logo {
+      font-size: 2.5rem;
+    }
+    .URLfield {
+      width: 80vw;
+    }
+    .button-area {
+      height: 40vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+    }
+    .linkform {
+      display: flex;
+      flex-direction: column;
+    }
+    .input-fields {
+      height: 20vh;
+      display: flex;
+      flex-direction: column;
+    }
   }
-  width: 40vw;
+  width: 45vw;
   height: 70vh;
-  border: 2px solid black;
+  /* border: 2px solid black; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   border-radius: 20px;
+  .logo {
+    font-size: 3rem;
+  }
+  .button-area {
+    padding-top: 30px;
+    display: flex;
+    justify-content: space-evenly;
+  }
+  .input-fields {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  .linkform {
+    padding-top: 20px;
+  }
 `;
 
 const DocsWrapper = styled.div`
@@ -37,8 +77,11 @@ const DocsWrapper = styled.div`
   }
   width: 30vw;
   height: 70vh;
-  border: 2px solid black;
+  border: 4px solid black;
   border-radius: 20px;
+  background-color: #ffdfa1;
+  display: flex;
+  justify-content: center;
 `;
 
 export { ParentWrapper, ChildWrapper, LinkWrapper, DocsWrapper };
