a9c2fb446a7edfb383ae0a8f683a477d745b5f13
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-12-29 15:42:11 +0530

    Routing Setup

--- Client - React/v2/src/index.js
+++ Client - React/v2/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./page";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <>
-      <HomePage />
-    </>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/users" element={<HomePage />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById("root")
 );
