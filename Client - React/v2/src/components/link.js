3ec7f840dede5c7728764e0b884c6a491c9a7b6c
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-12-30 12:28:26 +0530

    Some Minor Changes
    1. Added Icon Button
    -> Clipboard
    -> Random Short ID
    -> Clear
    
    2. Toast Notification Setup using React-Toastify
    
    3. Tooltip For Icon Buttons

--- Client - React/v2/src/components/link.js
+++ Client - React/v2/src/components/link.js
@@ -1,7 +1,26 @@
 import { LinkWrapper } from "../styles/layout-styles";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import ContentPasteOutlinedIcon from "@mui/icons-material/ContentPasteOutlined";
+import HighlightOffOutlinedIcon from "@mui/icons-material/HighlightOffOutlined";
+import ShuffleOutlinedIcon from "@mui/icons-material/ShuffleOutlined";
+import Tooltip from "@mui/material/Tooltip";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+import { nanoid } from "nanoid";
+import { useState } from "react";
 const LinkSection = () => {
+  const navigate = useNavigate();
+
+  const [shortID, setShortID] = useState("");
+  const [longURL, setLongURL] = useState(
+    "https://github.com/vigneshshettyin/EatMyURL"
+  );
+
+  const notify = () => toast("Copied to clipboard 📋");
+
   return (
     <LinkWrapper className="container-fluid">
       <div className="row">
@@ -18,17 +37,64 @@ const LinkSection = () => {
           <TextField
             className="container-fluid mt-2 mb-2"
             id="outlined-basic"
+            value={longURL}
+            onChange={(e) => setLongURL(e.target.value)}
+            defaultValue={longURL}
             label="Long URL"
             variant="outlined"
           />
         </div>
         <div class="col-6 col-md-4 border-1">
           <TextField
+            onChange={(e) => setShortID(e.target.value)}
+            value={shortID}
+            defaultValue={shortID}
             className="container-fluid mt-2 mb-2"
             id="outlined-basic"
             label="Short ID"
             variant="outlined"
           />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-4 buttons-material">
+          <Tooltip placement="top" title="Copy To Clipboard">
+            <IconButton
+              onClick={notify}
+              aria-label="ContentPasteOutlinedIcon"
+              size="large"
+            >
+              <ContentPasteOutlinedIcon />
+            </IconButton>
+          </Tooltip>
+        </div>
+        <div class="col-4 buttons-material">
+          <Tooltip placement="top" title="Generate Short ID">
+            <IconButton
+              onClick={() => {
+                setShortID(nanoid(7));
+              }}
+              aria-label="ShuffleOutlinedIcon"
+              size="large"
+            >
+              <ShuffleOutlinedIcon />
+            </IconButton>
+          </Tooltip>
+        </div>
+        <div class="col-4 buttons-material">
+          <Tooltip placement="top" title="Clear Text">
+            <IconButton
+              onClick={() => {
+                setLongURL("");
+                setShortID("");
+              }}
+              aria-label="HighlightOffOutlinedIcon"
+              size="large"
+            >
+              <HighlightOffOutlinedIcon />
+            </IconButton>
+          </Tooltip>
         </div>
       </div>
 
@@ -39,11 +105,18 @@ const LinkSection = () => {
           </Button>
         </div>
         <div class="col-6 buttons-material">
-          <Button variant="contained" size="large">
+          <Button
+            onClick={() => {
+              navigate("/click");
+            }}
+            variant="contained"
+            size="large"
+          >
             Visitor Clicks ?
           </Button>
         </div>
       </div>
+      <ToastContainer closeButton={false} />
     </LinkWrapper>
   );
 };
