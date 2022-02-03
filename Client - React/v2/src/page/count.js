1f3667a5b8fa04295ee937bdc2b4a298c787ffd3
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2022-02-03 08:39:32 +0530

    Bug Fixed!
    Line 9, 10, 13, 14

--- Client - React/v2/src/page/count.js
+++ Client - React/v2/src/page/count.js
@@ -1,17 +1,15 @@
 import { ParentWrapper, ChildWrapper } from "../styles/layout-styles";
 import Docs from "../components/docs";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import ClickSection from "../components/click";
 import { useState, useEffect } from "react";
 import Loading from "./loading";
 import Fade from "react-reveal/Fade";
 const CountLink = () => {
-  const navigate = useNavigate();
   const { click } = useParams();
 
   if (click !== "click") {
-    window.open(`https://eurl.tech/${click}`, "self");
-    navigate("/");
+    window.open(`https://eurl.tech/${click}`, "_parent");
   }
 
   const [loading, setLoading] = useState(true);
