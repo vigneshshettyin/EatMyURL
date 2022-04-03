baeed8e3312b01aac278258ecb831fc97cabe04c
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2022-04-03 10:19:06 +0530

    Docs!

--- Client - React/v2/src/components/docs.js
+++ Client - React/v2/src/components/docs.js
@@ -1,10 +1,12 @@
-import { serverURL } from "../api/request";
 import { DocsWrapper } from "../styles/layout-styles";
 
 const Docs = () => {
   return (
     <DocsWrapper>
-      <iframe title="EatMyURL API Docs" src={`${serverURL}/api-docs/`}></iframe>
+      <iframe
+        title="EatMyURL API Docs"
+        src="https://eurl.vigneshcodes.in/api-docs/"
+      ></iframe>
     </DocsWrapper>
   );
 };
