f792945a03aeb33dedead667abf67fc83c4ac6f0
Vignesh Shetty <vigneshshetty.in@gmail.com>
2024-04-27 22:15:09 +0530

    Intefaces for LinkCards

--- app/(dashboard)/links/page.tsx
+++ app/(dashboard)/links/page.tsx
@@ -2,24 +2,28 @@ import { DatePickerWithRange } from "@/components/DialogComponents/DatePickerWit
 import React from "react";
 import { FilterDialog } from "@/components/DialogComponents/FilterDialog";
 import { LinkCard } from "@/components/CardComponents/LinkCard";
+import { LinkCardProps } from "@/interfaces/ui";
 
-const data = [{
-  id:1,
-  title : "Rick Rolling",
-  shortLink : "eurl.dev/hello",
-  longLink : "https://google.com"
-},{
-  id:2,
-  title : "Blah Rolling",
-  shortLink : "eurl.dev/blaha",
-  longLink : "https://goo.com"
-},
-{
-  id:3,
-  title : "cutew Rolling",
-  shortLink : "eurl.dev/cuteww",
-  longLink : "https://goocut.com"
-}]
+const data : LinkCardProps[] = [
+  {
+    id: 1,
+    title: "Rick Rolling",
+    shortLink: "eurl.dev/hello",
+    longLink: "https://google.com",
+  },
+  {
+    id: 2,
+    title: "Blah Rolling",
+    shortLink: "eurl.dev/blaha",
+    longLink: "https://goo.com",
+  },
+  {
+    id: 3,
+    title: "cutew Rolling",
+    shortLink: "eurl.dev/cuteww",
+    longLink: "https://goocut.com",
+  },
+];
 
 export default function Page() {
   return (
@@ -27,15 +31,16 @@ export default function Page() {
       <h1 className="font-bold text-3xl ml-3">Links</h1>
       <div className="flex mt-6 md:flex-row flex-col">
         <div className="ml-2">
-        <DatePickerWithRange/>
+          <DatePickerWithRange />
         </div>
         <div className="mt-3 md:mt-0 ml-0 md:ml-4">
-          <FilterDialog/>
-        </div>  
+          <FilterDialog />
+        </div>
       </div>
-      
-      {data.map((link)=><LinkCard key={link.id} link={link}/>)}
-      
+
+      {data.map((link) => (
+        <LinkCard key={link.id} link={link} />
+      ))}
     </div>
   );
 }
