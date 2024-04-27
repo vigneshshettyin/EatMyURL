d3573335bababa719acff736667de5b564c79f1d
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-04-27 20:16:08 +0530

    link pages ui

--- app/(dashboard)/links/[id]/page.tsx
+++ app/(dashboard)/links/[id]/page.tsx
@@ -1,8 +1,79 @@
+"use client";
 
-export default function Page({params}:any) {
+import { LinkCard } from "@/components/CardComponents/LinkCard";
+import { LinkCardComponent } from "@/components/CardComponents/LinkCardDemoComponent";
+import { LinkCardSpecific } from "@/components/CardComponents/LinkCardSpecific";
+import { ArrowLeft, ArrowUpDownIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Label } from "@radix-ui/react-label";
+
+export default function Page({ params }: any) {
   const id = params.id;
+  const router = useRouter();
 
-  return <div className="h-screen">
-      
-  </div>
+  return (
+    <div className="h-screen">
+      <div
+        className="flex ml-10 cursor-pointer w-fit"
+        onClick={() => router.push("/links")}
+      >
+        <ArrowLeft size={20} />
+        <h1 className="ml-2 text-sm font-medium">Back to list</h1>
+      </div>
+      <LinkCardSpecific
+        link={{
+          title: "Main Page - Algorithms for Competitive Programming",
+          shortLink: "bit.ly/cphead_only",
+          longLink: "https://cp-algorithms.com/",
+        }}
+      />
+      <div className="flex md:flex-row flex-col ml-8 mt-6 mr-8">
+        <div className="flex-1 flex justify-between shadow-md p-4 rounded-xl items-center border-[0.5px]">
+          <h1 className="text-md">Engagements</h1>
+          <h1 className="text-2xl font-bold">7</h1>
+        </div>
+        <div className="flex-1 flex justify-between shadow-md ml-0 md:mt-0 mt-4 md:ml-4 p-4 rounded-xl items-center border-[0.5px]">
+          <h1 className="text-md">Last 7 days</h1>
+          <h1 className="text-2xl font-bold">10</h1>
+        </div>
+        <div className="flex-1 flex justify-between shadow-md ml-0 md:mt-0 mt-4 md:ml-4 p-4 rounded-xl items-center border-[0.5px]">
+          <h1 className="text-md">Weekly change</h1>
+          <div className="flex items-center">
+            <ArrowUpDownIcon size={25} />
+            <h1 className="text-2xl font-bold ml-1">+100%</h1>
+          </div>
+        </div>
+      </div>
+      <div className="mx-8 mt-6 border-[0.5px] rounded-xl px-3 py-4 shadow-md">
+        <Label className="font-bold ml-2">Locations</Label>
+        <Table className="mt-4">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">#</TableHead>
+              <TableHead>Country</TableHead>
+              <TableHead className="text-right">Engagements</TableHead>
+              <TableHead className="text-right">%</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>India</TableCell>
+              <TableCell className="text-right">5</TableCell>
+              <TableCell className="text-right">100%</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 }
---
+++ components/CardComponents/LinkCardSpecific.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import { BarChart2, Calendar, Copy, LinkIcon, Pencil, Share2 } from "lucide-react";
+import { LinkShareDialog } from "../DialogComponents/LinkShareDialog";
+import { Button } from "../ui/button";
+import { EditLinkDialog } from "../DialogComponents/EditLinkDialog";
+import { toast } from "../ui/use-toast";
+
+export function LinkCardSpecific({
+    link,
+  }: {
+    link: {
+      title: string;
+      shortLink: string;
+      longLink: string;
+    };
+  }){
+
+    function copyToClipboard(){
+        navigator.clipboard.writeText(link.shortLink);
+        toast({
+          title: "Copied the link to clipboard !!",
+        });
+      }
+
+    return <div className="flex mt-6 py-6 pr-4 mx-6 flex-col rounded-xl border-[0.5px] shadow-md">
+    <div className="flex">
+      <div className="flex flex-col ml-6 w-full">
+        <div className="flex justify-between ">
+          <h1 className="text-xl font-bold">
+            {link.title}
+          </h1>
+          <div className="hidden md:block">
+            <Button onClick={copyToClipboard} variant="outline">
+              <Copy size={15} className="mr-2" />
+              Copy
+            </Button>
+            <LinkShareDialog link={link}>
+            <Button variant="outline" className="ml-2">
+              <Share2 size={15} className="mr-2" />
+              Share
+            </Button>
+            </LinkShareDialog>
+            <EditLinkDialog>
+              <Button variant="outline" className="ml-2">
+                <Pencil size={15} className="mr-2" />
+                Edit
+              </Button>
+            </EditLinkDialog>
+          </div>
+        </div>
+        <div className="flex">
+            <div className="h-8 w-8 md:h-12 md:w-12 border-[0.5px] shadow-md rounded-full flex justify-center items-center">
+            <LinkIcon className="h-4 w-4 md:h-6 md:w-6" />
+            </div>
+            <div className="ml-4">
+                <h1 className="text-blue-400 mt-1 hover:underline cursor-pointer w-fit">
+                <a href={"https://"+link.shortLink}>{link.shortLink}</a>
+                </h1>
+                <h1 className="mt-2 text-sm hover:underline cursor-pointer w-fit">
+                <a href={link.longLink}>{link.longLink}</a>
+                </h1>
+            </div>
+        </div>
+        
+        <div className="flex mt-6 md:flex-row flex-col">
+          <div className="flex mt-2 md:mt-0">
+            <Calendar className="ml-0 md:ml-4 " size={20} />
+            <h1 className="text-sm ml-2">April 21 2024</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="mt-8 ml-8 md:hidden">
+        <Button onClick={copyToClipboard} variant="outline">
+          <Copy size={15} />
+        </Button>
+        <LinkShareDialog link={link}>
+        <Button variant="outline" className="ml-2">
+          <Share2 size={15} />
+        </Button>
+        </LinkShareDialog>
+        <EditLinkDialog>
+        <Button className="ml-2 " variant="outline">
+          <Pencil size={15}/>
+        </Button>
+        </EditLinkDialog>
+      </div>
+    </div>
+}
