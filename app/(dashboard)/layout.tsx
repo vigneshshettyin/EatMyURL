6e1d774c6aec4048a8bd26a37cbbbbcacc52efc3
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-04-27 17:21:13 +0530

    fixes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({children}:{
 }){
     return <div className="flex pb-4">
         <SideNavBar/>
-        <div className="ml-[40px] md:ml-[150px] w-full mt-[46px]">
+        <div className="ml-[40px] md:ml-[150px] w-full ">
         {children}
         </div>
     </div>
