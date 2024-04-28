a88954faecc662b7561714af2d433c2539adaa28
Vignesh Shetty <vigneshshetty.in@gmail.com>
2024-04-28 22:21:10 +0530

    refactor, Sentry imports

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { NextPage } from "next";
-import * as Sentry from "@sentry/nextjs";
-import Error from "next/error";
 import { useEffect } from "react";
-import exp from "constants";
+import Error from "next/error";
+
+import * as Sentry from "@sentry/nextjs";
+
+
 
 
 const GlobalError: NextPage<{ error: Error }> = ({ error }) => {
