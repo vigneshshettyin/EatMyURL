551935faee764cd2a6a95d4c727a20e0b4766e05
Vignesh Shetty <vigneshshetty.in@gmail.com>
2024-04-28 12:21:43 +0530

    Next response

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,13 +1,14 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req : NextRequest) {
 
     const userAgent = req.headers.get('user-agent');
     const ip = req.headers.get('x-real-ip') || req.headers.get('x-forwarded-for') || req.ip;
 
-    return new Response(JSON.stringify({ message: 'pong', userAgent, ip }), {
+    return  new NextResponse(JSON.stringify({ message: 'pong', userAgent, ip }), {
         headers: {
             'Content-Type': 'application/json'
         }, status: 200
     });
+
 }
