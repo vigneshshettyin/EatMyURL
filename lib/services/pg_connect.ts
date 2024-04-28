677a289ece5a1db5e7c4e772f6f3a42e57832bf0
Vignesh Shetty <vigneshshetty.in@gmail.com>
2024-04-28 19:38:17 +0530

    (Core) - For handling user agent data

--- lib/services/pg_connect.ts
+++ lib/services/pg_connect.ts
@@ -2,9 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
-export const getPrisma = () => {
-  if (!prisma) {
+const getPrisma = (): PrismaClient => {
+  if (!!prisma) {
     prisma = new PrismaClient();
   }
   return prisma;
 };
+
+export default getPrisma;
