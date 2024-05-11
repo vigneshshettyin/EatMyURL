40f3ea2ce361bf432b1bc122111ec900a2b45404
Vignesh Shetty <vigneshshetty.in@gmail.com>
2024-05-11 16:54:56 +0530

    Created Public Redis based APIs

--- lib/services/redis_connect.ts
+++ lib/services/redis_connect.ts
@@ -1,13 +1,27 @@
 import Redis from "ioredis";
-const redisUri: any = process.env.REDIS_URL;
+const RedisURI_01: any = `${process.env.REDIS_URL}/0`;
+const RedisURI_02: any = `${process.env.REDIS_URL}/1`;
+
 
 let redis: Redis;
+let public_redis : Redis;
 
 const getRedis = (): Redis => {
   if (!redis) {
-    redis = new Redis(redisUri);
+    redis = new Redis(RedisURI_01);
   }
   return redis;
 };
 
+const getPublicRedis = (): Redis => {
+  if (!public_redis) {
+    public_redis = new Redis(RedisURI_02);
+  }
+  return public_redis;
+};
+
 export default getRedis;
+
+export {
+  getPublicRedis
+}
---
+++ lib/validations/url_create.ts
@@ -0,0 +1,25 @@
+import { NextRequest } from "next/server";
+import { urlSchema } from "../zod/url";
+
+import { IUrlCreateReq } from "@/interfaces/url";
+
+const ValidateURLCreateReq = async (req: NextRequest) : Promise<IUrlCreateReq> => {
+    try {
+      const form_data: FormData = await req.formData();
+      const long_url = form_data.get("long_url");
+  
+      const errors = urlSchema.safeParse({
+        long_url,
+      });
+      return { long_url, status: errors.success, msg: errors.error };
+    } catch (e) {
+      return {
+        long_url: "",
+        status: false,
+        msg : JSON.stringify(e)
+      };
+    }
+  };
+
+
+export default ValidateURLCreateReq;
