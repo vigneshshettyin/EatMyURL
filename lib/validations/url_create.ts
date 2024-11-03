355618e872197767b8b4653842be1081a4ac61f6
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-11-03 17:31:34 +0530

    url_create bug fixed

--- lib/validations/url_create.ts
+++ lib/validations/url_create.ts
@@ -1,29 +1,14 @@
 import { urlSchema } from "../zod/url";
-import cheerio from 'cheerio';
 
 const validateURLCreateReq = async (formdata: FormData) => {
   try {
     const long_url = formdata.get("longUrl");
-    let title:any = formdata.get("title");
+    let title:any = "New Link";
 
     // validation of title need to be done
     const errors = urlSchema.safeParse({
       long_url,
     });
-
-    const urlHit: any = long_url;
-
-    if (title?.toString().length == 0) {
-        const response = await fetch(urlHit);
-        const html = await response.text();
-        const $ = cheerio.load(html);
-        const newTitle = $('title').text();
-        title = newTitle;
-
-        if(title?.toString().length == 0){
-            title = "New Link"
-        }
-    }
 
     return { title, long_url, status: errors.success, msg: errors.error };
   } catch (e) {
