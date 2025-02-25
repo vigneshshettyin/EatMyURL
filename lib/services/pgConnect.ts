2370a4871c46283af9fb0bf8a0ad8cf7c618802f
Vignesh Shetty <vigneshshetty.in@gmail.com>
2025-02-25 23:19:23 +0530

    Revert "enabled accelerate"
    This reverts commit be0dce7b044fb8f4ab4c848c6f57b43cae84fbab.

--- lib/services/pgConnect.ts
+++ lib/services/pgConnect.ts
@@ -1,6 +1,11 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
