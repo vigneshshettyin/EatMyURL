061da31e996098e6e2c661c783a623e5cc9a3bcb
Vignesh Shetty <vigneshshetty.in@gmail.com>
2025-02-25 22:45:15 +0530

    Connection limit for Pg

--- lib/services/pgConnect.ts
+++ lib/services/pgConnect.ts
@@ -1,5 +1,11 @@
-import PrismaClientManager from "./pgSingelton";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = PrismaClientManager.getInstance().getPrismaClient();
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
