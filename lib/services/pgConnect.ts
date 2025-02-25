be0dce7b044fb8f4ab4c848c6f57b43cae84fbab
Vignesh Shetty <vigneshshetty.in@gmail.com>
2025-02-25 23:14:06 +0530

    enabled accelerate

--- lib/services/pgConnect.ts
+++ lib/services/pgConnect.ts
@@ -1,11 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 export default prisma;
